98aedbbfd5ab547b4790cb71c060c632809f8b66
Ishan <ishan18madras@gmail.com>
2020-07-22 08:38:58 +0530

    why changes aren't

--- src/components/channels/LoadingSpinner.js
+++ src/components/channels/LoadingSpinner.js
@@ -4,21 +4,21 @@ const LoadingSpinner = () => {
   return (
     <>
       <div className="spinner-grow text-primary" role="status">
-        <span className="sr-only">Loading...</span>
+        <span className="sr-only">Loading..</span>
       </div>
       <div className="spinner-grow text-success" role="status">
-        <span className="sr-only">Loading...</span>
+        <span className="sr-only">Loading..</span>
       </div>
       <div className="spinner-grow text-danger" role="status">
-        <span className="sr-only">Loading...</span>
+        <span className="sr-only">Loading..</span>
       </div>
       <div className="spinner-grow text-warning" role="status">
-        <span className="sr-only">Loading...</span>
+        <span className="sr-only">Loading..</span>
       </div>
       <div className="spinner-grow text-info" role="status">
-        <span className="sr-only">Loading...</span>
+        <span className="sr-only">Loading..</span>
       </div>
-      <h1>Please wait for the data to be fetched. ... ...</h1>
+      <h1>Please wait for the data to be fetched.........</h1>
       <h1>
         If you're still waiting, A lot of suff is happening in the background
       </h1>
